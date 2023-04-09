fcd4e39278511a05382bf74ca569f991df1fe4a4
enoripl <tomek00@gmail.com>
2023-04-09 12:05:33 +0200

    finished development section of tutorial

---
+++ .history/src/index_20230407121042.js
@@ -0,0 +1,10 @@
+import myName from './myName';
+
+function component() {
+    const element = document.createElement('div')
+
+    element.textContent = myName('Cody')
+    return element;
+}
+
+document.body.appendChild(component());
